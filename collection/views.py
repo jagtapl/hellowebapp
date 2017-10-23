46f669480a578e65c7968ce008bc3d822ece43b3
LALIT JAGTAP <jagtap_lalit@yahoo.com>
2017-10-23 16:29:13 -0500

    template tags

--- collection/views.py
+++ collection/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     # this is your new view
-    return render(request, 'index.html')
+    # defining the variable
+    number = 6
+
+    #enclose string in quotes
+    thing = "Thing name"
+
+    # passing the variable to the view
+    return render(request, 'index.html', {
+	'number': number,
+        'thing' : thing,
+    })
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -21,10 +21,17 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
 """
-
+from django.views.generic import TemplateView
 from collection import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
+    # The new URL entries we're adding:
+    url(r'^about/$',
+        TemplateView.as_view(template_name='about.html'),
+        name='about'),
+    url(r'^contact/$',
+        TemplateView.as_view(template_name='contact.html'),
+        name='contact'),
     url(r'^admin/', admin.site.urls),
 ]
