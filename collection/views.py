fe7df42bd6931177f5eb0a82504a67165f0f1c2d
LALIT JAGTAP <jagtap_lalit@yahoo.com>
2017-10-25 19:12:43 -0500

    assoc users with objects

--- collection/views.py
+++ collection/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 
 from collection.forms import ThingForm
 from collection.models import Thing
+from django.template.defaultfilters import slugify
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # the rewritten view!
 def index(request):
@@ -25,9 +28,15 @@ def thing_detail(request, slug):
     })
 
 # our edit view
+@login_required
 def edit_thing(request, slug):
     # grab the object
     thing = Thing.objects.get(slug=slug)
+
+    # make sure the logged in user is the owner of thing
+    if thing.user != request.user:
+        raise Http404
+
     # set the form we're using
     form_class = ThingForm
 
@@ -49,5 +58,32 @@ def edit_thing(request, slug):
         'thing': thing,
         'form' : form
     })
+# our create view
+def create_thing(request):
+    form_class = ThingForm
 
+    # if we're coming from a submitted form, do this
+    if request.method == 'POST':
+        # grab the data from the submitted form and 
+        # apply to the form
+        form = form_class(request.POST)
+        if form.is_valid():
+            # create an instance but don't save yet
+            thing = form.save(commit=False)
 
+            # set the additional details
+            thing.user = request.user
+            thing.slug = slugify(thing.name)
+
+            # save the object
+            thing.save()
+
+            # redirect to our newly created thing
+            return redirect('thing_detail', slug=thing.slug)
+    # otherwise just create the form
+    else:
+        form = form_class()
+
+    return render(request, 'things/create_thing.html', {
+    'form': form,
+    })
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -13,16 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from collection.backends import MyRegistrationView
+from django.conf.urls import url, include
 from django.contrib import admin
-
-"""
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
-"""
 from django.views.generic import TemplateView
 from collection import views
+from django.contrib.auth.views import (
+    password_reset,
+    password_reset_done,
+    password_reset_confirm,
+    password_reset_complete    
+)
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
@@ -37,5 +38,33 @@ urlpatterns = [
         name='thing_detail'),
     url(r'^things/(?P<slug>[-\w]+)/edit/$', views.edit_thing,
         name='edit_thing'),
+    # the new password reser URLs
+    url(r'^accounts/password/reset/$',
+        password_reset,
+        {'template_name':
+        'registration/password_reset_form.html'},
+        name="password_reset"),
+    url(r'^accounts/password/reset/done/$',
+        password_reset_done,
+        {'template_name':
+        'registration/password_reset_done.html'},
+        name="password_reset_done"),
+    url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        password_reset_confirm,
+        {'template_name':
+        'registration/password_reset_confirm.html'},
+        name="password_reset_confirm"),
+    url(r'^accounts/password/done/$',
+        password_reset_complete,
+        {'template_name':
+        'registration/password_reset_complete.html'},
+        name="password_reset_complete"),
+    url(r'^accounts/register/$',
+        MyRegistrationView.as_view(),
+        name='registration_register'),
+    url(r'^accounts/create_thing/$', views.create_thing,
+        name='registration_create_thing'),
+    url(r'^accounts/',
+        include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
 ]
